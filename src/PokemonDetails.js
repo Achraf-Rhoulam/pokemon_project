77f236e3be2509f5649b231b6f2d928158d64ce3
Achraf-Rhoulam <achrafrhoulam1@gmail.com>
2024-04-22 12:21:57 +0200

    Création de fichier Pokemon Details qui permet de détailler les infos sur mes pokémons

---
+++ src/PokemonDetails.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const PokemonDetails = ({ pokemon }) => {
+  if (!pokemon) {
+    return <div>Sélectionnez un Pokémon pour afficher les détails.</div>;
+  }
+
+  return (
+    <div>
+      <h2>Détails de {pokemon.name}</h2>
+      <p>Numéro : {pokemon.id}</p>
+      <p>Type : {pokemon.types.map(type => type).join(', ')}</p>
+      <img src={pokemon.image} alt={pokemon.name} />
+    </div>
+  );
+};
+
+export default PokemonDetails;
